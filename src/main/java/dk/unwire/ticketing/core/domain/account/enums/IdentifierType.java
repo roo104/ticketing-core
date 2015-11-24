19e1ac098b6fc63fbb6561ec853b1c453b1b517b
James Treacy <jat@unwire.dk>
2015-11-24 13:00:12 +0100

    [MTICKET-6214] Removing Lombok annotations as Sonar has a bug which means Lombok confuses it causing it to break the build, the fix for this has not been released yet by SonarQube (see: https://jira.sonarsource.com/browse/SONARJAVA-1360)

--- src/main/java/dk/unwire/ticketing/core/domain/account/enums/IdentifierType.java
+++ src/main/java/dk/unwire/ticketing/core/domain/account/enums/IdentifierType.java
@@ -1,16 +1,22 @@
 package dk.unwire.ticketing.core.domain.account.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
 public enum IdentifierType {
-    MSISDN(1),
+
+	MSISDN(1),
     LICENSE_PLATE(4),
     EMAIL(8);
-    @Getter
+
+	IdentifierType() {
+
+	}
+
+	IdentifierType(int id) {
+		this.id = id;
+	}
+
     private int id;
 
+	public int getId() {
+		return this.id;
+	}
 }
